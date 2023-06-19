f77d000928bc61fa75762a06e59ccfbe788c5cda
Sanjay Khandla <sanjay.khandla@solutionanalysts.in>
2023-06-19 18:34:39 +0530

    add the word, charecter count

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 function App() {
   return (
     <>
-      <Navbar title="TextUtils" About="about" />
+      <Navbar title="TextUtils" About="About" />
+      <div className="container my-3">
+        <TextForm Text="Enter your text"/>
+        
+      </div>
     </>
   );
 }
